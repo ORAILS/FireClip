8bf203f3a7946bb9b4475426b61188c2c75bc1d3
DanielRailean <daniel.railean1@gmail.com>
2023-12-16 16:32:33 +0100

    Fixed build bug.

--- bugs.md
+++ bugs.md
@@ -2,3 +2,5 @@
 
 - search -> try to paste from keyboard
 - after search paste -> field not cleared  ( maybe setting )
+- hover is enabled when scrolling / trying to paste
+- build date is static
---
+++ validate_arch.ts
@@ -0,0 +1,16 @@
+import { execSync } from 'child_process'
+
+const main = async () => {
+    const arch = execSync('arch', { encoding: 'utf-8' }).trim()
+    const archNode = execSync('node -e "console.log(process.arch)"', { encoding: 'utf-8' }).trim()
+    console.log(`arch: '${arch}'`)
+    console.log(`node arch: '${archNode}'`)
+    if (arch != archNode) {
+        if (arch == 'i386' && archNode == 'x64') {
+            return
+        }
+        throw new Error('OS/Node arch are diferent, build will fail!')
+    }
+}
+
+main()
