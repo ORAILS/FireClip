d2857cd6aaa286064ba991896b27318a836b36d7
Daniel Railean <daniel.railean1@gmail.com>
2024-05-21 19:05:56 +0200

    automatic image generation during build.

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -18,7 +18,7 @@ app.on('ready', async () => {
         autoUpdater.checkForUpdatesAndNotify()
     }, 5 * 60 * 1000)
 
-    const iconPath = path.join(__dirname, 'www', 'logo_32.png')
+    const iconPath = path.join(__dirname, 'www', 'icons/png/32x32.png')
     const icon = nativeImage.createFromPath(iconPath)
 
     const appIcon = new Tray(icon)
